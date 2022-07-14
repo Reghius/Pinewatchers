7973fc90c583183091e59a2f9c59fe17cf69f34d
Reghius <szylak.m@gmail.com>
2022-07-14 23:41:07 +0200

    refactor: simplified names of models

--- backend/robots/admin.py
+++ backend/robots/admin.py
@@ -29,33 +29,33 @@ class RobotManufacturerAdmin(admin.ModelAdmin):
 @admin.register(CommunicationDevice)
 class CommunicationDeviceAdmin(admin.ModelAdmin):
     list_display = (
-        'device_id',
-        'device_x_size',
-        'device_y_size',
-        'device_z_size'
+        'name',
+        'x_size',
+        'y_size',
+        'z_size'
     )
 
 
 @admin.register(Robot)
 class RobotAdmin(admin.ModelAdmin):
-    list_select_related = ('client', 'robot_type', 'communication_device_id')
+    list_select_related = ('client', 'robot_type', 'communication_device_name')
     list_display = (
-        'robot_name',
-        'robot_owner',
-        'robot_manufacturer',
-        'robot_serial_number',
-        'robot_production_date',
-        'robot_type',
-        'communication_device_id'
+        'name',
+        'owner',
+        'manufacturer',
+        'serial_number',
+        'production_date',
+        'type',
+        'communication_device_name'
     )
 
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_select_related = ('robot_name', 'communication_device_id')
+    list_select_related = ('robot_name', 'communication_device_name')
     list_display = (
         'robot_name',
-        'communication_device_id',
+        'communication_device_name',
         'timestamp',
         'latitude',
         'longitude'
@@ -64,10 +64,10 @@ class LocationAdmin(admin.ModelAdmin):
 
 @admin.register(Telemetry)
 class TelemetryAdmin(admin.ModelAdmin):
-    list_select_related = ('robot_name', 'communication_device_id')
+    list_select_related = ('robot_name', 'communication_device_name')
     list_display = (
         'robot_name',
-        'communication_device_id',
+        'communication_device_name',
         'timestamp',
         'humidity',
         'temperature',
--- backend/robots/models.py
+++ backend/robots/models.py
@@ -42,31 +42,31 @@ class RobotManufacturer(models.Model):
 
 
 class CommunicationDevice(models.Model):
-    device_id = models.CharField(max_length=20)
-    device_x_size = models.FloatField()
-    device_y_size = models.FloatField()
-    device_z_size = models.FloatField()
+    name = models.CharField(max_length=20)
+    x_size = models.FloatField()
+    y_size = models.FloatField()
+    z_size = models.FloatField()
 
     def __str__(self):
-        return f'{self.device_id}'
+        return f'{self.name}'
 
 
 class Robot(models.Model):
-    robot_name = models.CharField(max_length=20)
-    robot_owner = models.ForeignKey(Client, on_delete=models.CASCADE)
-    robot_manufacturer = models.CharField(max_length=20)
-    robot_serial_number = models.CharField(max_length=20)
-    robot_production_date = models.DateField()
-    robot_type = models.ForeignKey(RobotType, on_delete=models.CASCADE)
-    communication_device_id = models.ForeignKey(CommunicationDevice, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    owner = models.ForeignKey(Client, on_delete=models.CASCADE)
+    manufacturer = models.CharField(max_length=20)
+    serial_number = models.CharField(max_length=20)
+    production_date = models.DateField()
+    type = models.ForeignKey(RobotType, on_delete=models.CASCADE)
+    communication_device_name = models.ForeignKey(CommunicationDevice, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.robot_name}'
+        return f'{self.name}'
 
 
 class Location(models.Model):
     robot_name = models.ForeignKey(Robot, on_delete=models.CASCADE)
-    communication_device_id = models.ForeignKey(CommunicationDevice, on_delete=models.CASCADE)
+    communication_device_name = models.ForeignKey(CommunicationDevice, on_delete=models.CASCADE)
     timestamp = models.DateTimeField()
     latitude = models.FloatField()
     longitude = models.FloatField()
@@ -74,7 +74,7 @@ class Location(models.Model):
 
 class Telemetry(models.Model):
     robot_name = models.ForeignKey(Robot, on_delete=models.CASCADE)
-    communication_device_id = models.ForeignKey(CommunicationDevice, on_delete=models.CASCADE)
+    communication_device_name = models.ForeignKey(CommunicationDevice, on_delete=models.CASCADE)
     timestamp = models.DateTimeField()
     humidity = models.FloatField()
     temperature = models.FloatField()

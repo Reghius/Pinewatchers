598031922b87747af9d552ddaa602bd3dd7af534
Reghius <szylak.m@gmail.com>
2022-07-21 09:59:02 +0200

    feat: added urls to seperate file in robots app

--- backend/pinewatchers/urls.py
+++ backend/pinewatchers/urls.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from robots import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('robots/', views.get_robots, name='robots'),
-    path('robots/data/', views.get_robots_data, name='robots data'),
-    path('robot/<int:robot_id>', views.get_robot_data, name='get robot data'),
-    path('location/', views.get_location, name='location'),
-    path('telemetry/', views.get_telemetry, name='telemetry'),
-    path('lastlocation/', views.get_latest_location, name='latest location'),
-    path('modify/<int:robot_id>', views.modify_robot_brand, name='modify robot model'),
-    path('client/', views.add_new_client, name='add new client')
+    path('robotsapp/', include('robots.urls'))
 ]

dedd323d8dc7ed0e8da86168507abd8e37278970
Reghius <szylak.m@gmail.com>
2022-07-27 12:31:17 +0200

    chore: deleted robot name from added data in celery tasks

--- backend/robots/tasks.py
+++ backend/robots/tasks.py
@@ -7,16 +7,14 @@ from celery import shared_task
 
 
 @shared_task
-def process_location(robot_name, sensor_nr, location_str):
+def process_location(sensor_nr, location_str):
     seperated_location = textwrap.wrap(location_str, 16)
     timestamp = datetime.fromtimestamp(struct.unpack('>d', bytearray.fromhex(seperated_location[0])))
     latitude = struct.unpack('>d', bytearray.fromhex(seperated_location[1]))
     longitude = struct.unpack('>d', bytearray.fromhex(seperated_location[2]))
-    robot = Robot.objects.get(name=robot_name)
     sensor = CommunicationDevice.objects.get(name=sensor_nr)
 
     Location.objects.create(
-        robot_object = robot,
         communication_device = sensor,
         timestamp = timestamp,
         latitude = latitude,
@@ -25,16 +23,14 @@ def process_location(robot_name, sensor_nr, location_str):
 
 
 @shared_task
-def process_telemetry(robot_name, sensor_nr, telemetry_str):
+def process_telemetry(sensor_nr, telemetry_str):
     timestamp = datetime.fromtimestamp(struct.unpack('>d', bytearray.fromhex(telemetry_str[:16]))[0])
     humidity = int(telemetry_str[16:18], 16)
     temperature = int(telemetry_str[18:20], 16)
     pressure = int(telemetry_str[20:24], 16)
-    robot = Robot.objects.get(name=robot_name)
     sensor = CommunicationDevice.objects.get(name=sensor_nr)
 
     Telemetry.objects.create(
-        robot_object = robot,
         communication_device = sensor,
         timestamp=timestamp,
         humidity=humidity,

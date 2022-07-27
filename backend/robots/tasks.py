9a8cfecaf51564749658091912730443ebeb925d
Reghius <szylak.m@gmail.com>
2022-07-27 10:56:02 +0200

    feat: added task for processing location from data sent by robot/sensor

--- backend/robots/tasks.py
+++ backend/robots/tasks.py
@@ -1,9 +1,27 @@
+from time import time
+from robots.models import Telemetry, Location, Robot, CommunicationDevice
+import textwrap
+from datetime import datetime
+import struct
 from celery import shared_task
 
 
 @shared_task
-def process_location():
-    pass
+def process_location(robot_name, sensor_nr, location_str):
+    seperated_location = textwrap.wrap(location_str, 16)
+    timestamp = datetime.fromtimestamp(struct.unpack('>d', bytearray.fromhex(seperated_location[0])))
+    latitude = struct.unpack('>d', bytearray.fromhex(seperated_location[1]))
+    longitude = struct.unpack('>d', bytearray.fromhex(seperated_location[2]))
+    robot = Robot.objects.get(name=robot_name)
+    sensor = CommunicationDevice.objects.get(name=sensor_nr)
+
+    Location.objects.create(
+        robot_object = robot,
+        communication_device = sensor,
+        timestamp = timestamp,
+        latitude = latitude,
+        longitude = longitude
+    )
 
 
 @shared_task

79c62cc8a5f5a2f0a1a6a7c548f13ac9ac1f7898
Reghius <szylak.m@gmail.com>
2022-07-28 10:34:43 +0200

    feat: added location and telemetry tasks to consumer

--- backend/consumer.py
+++ backend/consumer.py
@@ -1,10 +1,13 @@
 import os
 import django
+import ast
 from django.conf import settings
 import paho.mqtt.client as mqtt
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pinewatchers.settings')
 django.setup()
+
+from robots.tasks import process_location, process_telemetry
 
 
 def on_connect(client, *args):
@@ -12,12 +15,22 @@ def on_connect(client, *args):
 
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    aux_topic = msg.topic.split('/')
+    aux_payload = ast.literal_eval(msg.payload.decode('ascii'))
+    dict_data = aux_payload['data']
+    sensor_name = aux_topic[1]
 
+    if aux_topic[2] == 'location':
+        process_location.delay(sensor_name, dict_data)
+    elif aux_topic[2] == 'telemetry':
+        process_telemetry.delay(sensor_name, dict_data)
+    else:
+        raise NotImplementedError('Unsupported message type.')
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.username_pw_set(username=settings.MQTT['LOGIN'], password=settings.MQTT['PASSWORD'])
-client.connect(settings.MQTT['HOST'], settings.MQTT['PORT'], 60)
-client.loop_forever()
+if __name__ == '__main__':
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.username_pw_set(username=settings.MQTT['LOGIN'], password=settings.MQTT['PASSWORD'])
+    client.connect(settings.MQTT['HOST'], settings.MQTT['PORT'], 60)
+    client.loop_forever()
--- backend/robots/tasks.py
+++ backend/robots/tasks.py
@@ -7,12 +7,12 @@ from celery import shared_task
 
 
 @shared_task
-def process_location(sensor_nr, location_str):
+def process_location(sensor_name, location_str):
     seperated_location = textwrap.wrap(location_str, 16)
     timestamp = datetime.fromtimestamp(struct.unpack('>d', bytearray.fromhex(seperated_location[0])))
     latitude = struct.unpack('>d', bytearray.fromhex(seperated_location[1]))
     longitude = struct.unpack('>d', bytearray.fromhex(seperated_location[2]))
-    sensor = CommunicationDevice.objects.get(name=sensor_nr)
+    sensor = CommunicationDevice.objects.get(name=sensor_name)
 
     Location.objects.create(
         communication_device = sensor,
@@ -23,12 +23,12 @@ def process_location(sensor_nr, location_str):
 
 
 @shared_task
-def process_telemetry(sensor_nr, telemetry_str):
+def process_telemetry(sensor_name, telemetry_str):
     timestamp = datetime.fromtimestamp(struct.unpack('>d', bytearray.fromhex(telemetry_str[:16]))[0])
     humidity = int(telemetry_str[16:18], 16)
     temperature = int(telemetry_str[18:20], 16)
     pressure = int(telemetry_str[20:24], 16)
-    sensor = CommunicationDevice.objects.get(name=sensor_nr)
+    sensor = CommunicationDevice.objects.get(name=sensor_name)
 
     Telemetry.objects.create(
         communication_device = sensor,

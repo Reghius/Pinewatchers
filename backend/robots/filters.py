50573525d4022d7c59ad7f11f8d2126163ce2f85
Reghius <szylak.m@gmail.com>
2022-08-06 13:27:21 +0200

    feat: added filter for showing latest location

--- backend/robots/filters.py
+++ backend/robots/filters.py
@@ -1,1 +1,11 @@
 import django_filters
+from robots.models import Location
+
+
+class LocationFilter(django_filters.FilterSet):
+    class Meta:
+        model = Location
+        fields = {
+            'robot': ['exact'],
+            'timestamp': ['timestamp__gte', 'timestamp__lte'],
+        }

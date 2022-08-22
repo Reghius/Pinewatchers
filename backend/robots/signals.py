bb15cbab178f095e456e8dabf01dc90c6424c83a
Reghius <szylak.m@gmail.com>
2022-08-22 18:52:48 +0200

    feat: added signal for saving robot modification

---
+++ backend/robots/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from robots.models import Robot, RobotModificationHistory
+
+
+@receiver(post_save, sender=Robot, dispatch_uid="save_changes")
+def save_changes(sender, instance, **kwargs):
+    print(instance)
+    RobotModificationHistory.objects.create(
+        name=instance.name,
+        owner=instance.owner,
+        manufacturer=instance.manufacturer,
+        serial_number=instance.serial_number,
+        production_date=instance.production_date,
+        type=instance.type
+        )
